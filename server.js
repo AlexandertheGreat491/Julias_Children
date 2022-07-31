3c103c39fdf9bf1c5ab78668a0210a92bc761f48
agvan <agvandyke25@gmail.com>
2022-07-31 16:16:52 -0500

    added logic

--- server.js
+++ server.js
@@ -1,13 +1,41 @@
+const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 
-const sequelize = require('./config/connection');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super duper secret secret',
+  cookie:{},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+//const helpers = require('./utils/helpers);
+
+//const hbs = exphbs.create({helpers});
+
+//app.engine('handlebars', hbs.engine);
+//app.set('view engine', 'handlebars');
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+//app.use(express.static(path.join(_dirname, 'public')));
 
+//app.use(require('./controllers/'));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
