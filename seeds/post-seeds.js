2295678ccf43e7218606b65b303eb7197efbb4e3
agvan <agvandyke25@gmail.com>
2022-08-02 16:00:23 -0500

    fixed seeds again

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -23,15 +23,6 @@ const postdata = [
     title: 'Amish Breakfast Casserole',
     genre: 'casserole',
     ingredients:
-<<<<<<< HEAD
-      "sliced bacon, medium sweet onion, large eggs, shredded hash brown potatoes, shredded cheddar cheese, cottage cheese, shredded Swiss cheese",
-    difficulty: "medium",
-    requirements: "Prep time: 15 minutes, Bake: 35 minutes + standing",
-    user_id: 1,
-  },
-  
-  
-=======
       '1 pound sliced bacon, diced, 1 medium sweet onion, chopped, 6 large eggs, lightly beaten, 4 cups frozen shredded hash brown potatoes thawed, 2 cups shredded cheddar cheese, 1-1/2 cups 4% cottage cheese, 1-1/4 shredded Swiss cheese',
     difficulty: 'medium',
     requirements: 'Prep time: 15 minutes, Bake: 35 minutes + standing',
@@ -55,112 +46,67 @@ const postdata = [
     requirements: 'Prep time: 20 minutes, Cook: 10 minutes',
     user_id: 7,
   },
->>>>>>> ee1c3911e49c2b848747d36bd827e04c6db1cc59
   {
     title: 'Apple Pie',
     genre: 'pie',
     ingredients:
-<<<<<<< HEAD
-      "pie dough, sugar, brown sugar, all-purpose flour, ground cinnamon, ground nutmeg, apples, lemon juice, butter, egg, cinnamon",
-    difficulty: "hard",
-    requirements:
-      "Prep time: 20 minutes + chilling, Bake: 45 minutes + cooling",
-=======
       'Dough for double-crust pie, 1/3 cup sugar, 1/3 cup packed brown sugar, 1/4 cup all-purpose flour, 1 teaspoon ground cinnamon, 1/4 teaspoon ground nutmeg, 6 to 7 cups thinly sliced peeled tart apples, 1 tablespoon lemon juice, 1 tablespoon butter, 1 large egg white, Optional: Turbinado or coarse sugar, ground cinnamon, vanilla bean ice cream, and caramel sauce',
     difficulty: 'hard',
     requirements: 'Prep time: 20 minutes + chilling, Bake: 45 minutes + cooling',
->>>>>>> ee1c3911e49c2b848747d36bd827e04c6db1cc59
     user_id: 4,
   },
   {
     title: 'Traditional Lasagna',
     genre: 'lasagna',
     ingredients:
-<<<<<<< HEAD
-      "ground beef, pork sausage, cans tomato sauce, tomato paste, garlic cloves minced, sugar, Italian seasoning, salt, pepper, large eggs, parsely, small-curd cottage cheese, ricotta cheese, grated Parmesean cheese, lasagna noodles, provolone Cheese, shredded mozzarella cheese",
-    difficulty: "hard",
-    requirements:
-      "Prep time: 30 minutes + simmering, Bake: 70 minutes + standing",
-=======
       '1 pound ground beef, 3/4 pound bulk pork sausage, 3 cans(8 ounces each) tomato sauce, 2 cans (6 ounces each) tomato paste, 2 garlic cloves minced, 2 teaspons sugar, 1 teaspoon Italian seasoning, 1/2 to 1 teaspoon salt, 1/4 to 1/2 teaspoon pepper, 3 large eggs, 3 tablespoons minced fresh parsely, 3 cups 4% small-curd cottage cheese, 1 cup ricotta cheese, 1/2 cup grated Parmesean cheese, 9 lasagna noodles cooked and drained, 6 slices Beliogioso Provolone Cheese (about 6 ounces), 3 cups shredded part-skim mozzarella cheese divided',
     difficulty: 'hard',
     requirements: 'Prep time: 30 minutes + simmering, Bake: 70 minutes + standing',
->>>>>>> ee1c3911e49c2b848747d36bd827e04c6db1cc59
     user_id: 1,
   },
   {
     title: "Li'l Cheddar Meat Loaves",
     genre: 'meat loaf',
     ingredients:
-<<<<<<< HEAD
-      "large egg, whole milk, shredded cheddar cheese, quick-cooking oats, chopped onion, salt, ground beef, ketchup, packed brown sugar, mustard",
-    difficulty: "medium",
-    requirements: "Prep time: 15 minutes, Bake: 25 minutes",
-=======
       '1 large egg, 3/4 cup whole milk, 1 cup shredded cheddar cheese, 1/2 quick-cooking oats, 1/2 cup quick-cooking oats, 1/2 cup chopped onion, 1/2 teaspoon salt, 1 pound ground beef, 2/3 cup ketchup, 1/2 cup packed brown sugar, 1-1/2 teaspoons prepared mustard',
     difficulty: 'medium',
     requirements: 'Prep time: 15 minutes, Bake: 25 minutes',
->>>>>>> ee1c3911e49c2b848747d36bd827e04c6db1cc59
     user_id: 9,
   },
   {
     title: 'Rustic Italian Tortellini Soup',
     genre: 'soup',
     ingredients:
-<<<<<<< HEAD
-      "Italian turkey sausage links, onion, garlic cloves, reduced-sodium chicken broth, water, diced tomatoes, cheese tortellini, baby spinach, minced basil, pepper, dash crushed red pepper flakes, shredded parmesean cheese",
-    difficulty: "easy",
-    requirements: "Prep time: 20 minutes, Cook: 20 minutes",
-=======
       '3/4 pound Italian turkey sausage links, casings removed, 1 medium onion, chopped, 6 garlic cloves, minced, 2 cans (14-1/2 ounces each) reduced-sodium chicken broth, 1-3/4 cups water, 1 can (14-1/2 ounces) diced tomatoes, undrained, 1 package (9 ounces) refrigerated cheese tortellini, 1 package (6 ounces) fresh baby spinach, coasrsely chopped, 2-1/4 teaspoons minced fresh basil or 3/4 teaspoon dried basil, 1/4 teaspoon pepper, dash crushed red pepper flakes, shredded parmesean cheese, optional',
     difficulty: 'easy',
     requirements: 'Prep time: 20 minutes, Cook: 20 minutes',
->>>>>>> ee1c3911e49c2b848747d36bd827e04c6db1cc59
     user_id: 5,
   },
   {
     title: 'Fluffy Key Lime Pie',
     genre: 'pie',
     ingredients:
-<<<<<<< HEAD
-      "boiling water, sugar-free lime gelatin, Key lime yogurt, frozen whipped topping thawed, graham cracker crust",
-    difficulty: "easy",
-    requirements: "Prep time: 20 minutes + chilling",
-=======
       '1/4 cup boiling water, 1 package (0.3 ounce) sugar-free lime gelatin, 2 cartons (6 ounces each) Key lime yogurt, 1 carton (8 ounces) frozen fat-free whipped topping thawed, 1 reduced-fat graham cracker crust(9 inches)',
     difficulty: 'easy',
     requirements: 'Prep time: 20 minutes + chilling',
->>>>>>> ee1c3911e49c2b848747d36bd827e04c6db1cc59
     user_id: 3,
   },
   {
     title: 'Pineapple Orange Cake',
     genre: 'cake',
     ingredients:
-<<<<<<< HEAD
-      "yellow cake mix, mandarin oranges, large egg whites, applesauce, crushed pineapple, instant vanilla pudding mix, whipped topping",
-    difficulty: "medium",
-    requirements: "Prep time: 15 minutes, Bake: 25 minutes",
-=======
       '1 package yellow cake mix(regular size), 1 can (11 ounces) mandarin oranges undrained, 4 large egg whites, 1/2 cup unsweetened applesauce, Topping: 1 can(20 ounces) crushed pineapple undrained, 1 package (1 ounce) sugar free instant vanilla pudding mix, 1 carton (8 ounces) reduced-fat whipped topping',
     difficulty: 'medium',
     requirements: 'Prep time: 15 minutes, Bake: 25 minutes',
->>>>>>> ee1c3911e49c2b848747d36bd827e04c6db1cc59
     user_id: 7,
   },
   {
     title: 'Baked Spaghetti',
     genre: 'pasta',
     ingredients:
-<<<<<<< HEAD
-      "onion, green pepper, butter, diced tomatoes undrained, mushroom, sliced ripe olives drained, dried oregano, ground beef, spaghetti, shredded cheddar cheese, cream of mushroom soup, water, grated parmesean cheese",
-    difficulty: "medium",
-    requirements: "Prep time: 30 minutes, Bake: 30 minutes",
-=======
       '1 cup chopped onion, 1 cup chopped green pepper, 1 tablespoon butter, 1 can(28 ounces) diced tomatoes undrained, 1 can (4 ounces) mushroom stems and pieces drained, 1 can (2-1/4 ounces) sliced ripe olives drained, 2 teaspoons dried oregano, 1 pound ground beef, browned and drained, optional, 12 ounces spaghetti cooked and drained, 2 cups shredded cheddar cheese, 1 can (10-3/4 ounces) condensed cream of mushroom soup undiluted, 1/4 cup water, 1/4 cup grated parmesean cheese',
     difficulty: 'medium',
     requirements: 'Prep time: 30 minutes, Bake: 30 minutes',
->>>>>>> ee1c3911e49c2b848747d36bd827e04c6db1cc59
     user_id: 6,
   },
   {
@@ -169,25 +115,13 @@ const postdata = [
     ingredients: '2/3 cup butter softened, 1 cup sugar, 3 large eggs room temperature, 1-2/3 cups 2% milk, 2-1/3 all-purpose flour, 1 cup cornmeal, 4-1/2 teaspoons baking powder, 1 teaspoon salt',
     difficulty: 'medium',
     requirements:
-<<<<<<< HEAD
-      "butter, sugar, eggs, 2% milk, all-purpose flour, cornmeal, baking powder, salt",
-=======
       'Preheat oven to 400°. In a large bowl, cream butter and sugar until light and fluffy, 5-7 minutes. Combine eggs and milk. Combine flour, cornmeal, baking powder and salt; add to creamed mixture alternately with egg mixture. Pour into a greased 13x9-in. baking pan. Bake 22-27 minutes or until a toothpick inserted in center comes out clean. Cut into squares; serve warm.',
->>>>>>> ee1c3911e49c2b848747d36bd827e04c6db1cc59
     user_id: 4,
   },
   {
     title: "Sandy's Chocolate Cake",
     genre: 'cake',
     ingredients:
-<<<<<<< HEAD
-      "butter, packed brown sugar, large eggs, vanilla extract, all-purpose flour, baking cocoa, baking soda, salt, sour cream, boiling water",
-    difficulty: "hard",
-    requirements: "Prep time: 30 minutes, Bake: 30 minutes + cooling",
-    user_id: 6,
-  },
-  
-=======
       '1 cup butter softened, 3 cups packed brown sugar, 4 large eggs room temperature, 2 teaspoons vanilla extract, 2-2/3 cups all-purpose flour, 3/4 cup baking cocoa, 3 teaspoons baking soda, 1/2 teaspoon salt, 1-1/3 cups sour cream, 1-1/3 cups boiling water',
     difficulty: 'hard',
     requirements: 'Prep time: 30 minutes, Bake: 30 minutes + cooling',
@@ -202,7 +136,6 @@ const postdata = [
     requirements: 'Prep time: 25 minutes + chilling',
     user_id: 7,
   },
->>>>>>> ee1c3911e49c2b848747d36bd827e04c6db1cc59
 ];
 
 const seedPosts = () => Post.bulkCreate(postdata);
