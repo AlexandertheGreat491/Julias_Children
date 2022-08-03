ac8c30364d6fb5b3653de296d027b4426fb195ee
Jordan D <degco44@gmail.com>
2022-08-02 20:03:14 -0500

    fill out login.js

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -28,4 +28,19 @@ async function signupFormHandler(event) {
     const username = document.querySelector()// username signup field here
     const email = document.querySelector() // email signup field here
     const password = document.querySelector() // password signup field here
+
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        document.querySelector('loginformgoeshere').addEventListener('submit', loginFormHandler);
+        document.querySelector('signupformgoeshere').addEventListener('submit', signupFormHandler);
+    }
 }
