6d57288cfa18cd6fdcfe193743eb133e96a57dd1
LoiTran <tranloi781@gmail.com>
2022-08-02 20:52:05 -0500

    small update

---
+++ public/javascript/add-post.js
@@ -0,0 +1,26 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector(/*post title area */).value;
+    const post_url = document.querySelector(/*post text area */).value;
+  
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        post_url
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+  
