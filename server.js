d87be37947163e2c19e1dc182eca8d1f6d530d20
J0J0C0DING <102717784+J0J0C0DING@users.noreply.github.com>
2022-08-04 23:52:08 -0500

    Updated server and config/connection.js, sequlize was not working

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const path = require('path');
 const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+const helpers = require('./utils/helpers');
+//const auth = require('../../utils/auth);
 
 const sequelize = require('./config/connection');
+const path = require('path');
+
+const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -19,23 +20,24 @@ const sess = {
   }),
 };
 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
 app.use(session(sess));
 
-//const auth = require('../../utils/auth);
-
-//const hbs = exphbs.create({helpers});
-
-//app.engine('handlebars', hbs.engine);
-//app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use(express.static(path.join(_dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+app.use(routes);
 
 // turn on connection to db and server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-  sequelize.sync({ force: false });
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
