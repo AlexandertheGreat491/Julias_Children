b408b43723a2c1cad0acbdfbbf0b73d44ae9eeb4
J0J0C0DING <102717784+J0J0C0DING@users.noreply.github.com>
2022-08-07 18:59:25 -0500

    Maybe fixed login >>>> dashboard issue, changed login.js query selectors

--- controllers/index.js
+++ controllers/index.js
@@ -4,13 +4,12 @@ const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes');
 const dashboardRoutes = require('./dashboard-routes');
 
-router.use('/api', apiRoutes);
+router.use('/api/', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
-
 
 module.exports = router;
