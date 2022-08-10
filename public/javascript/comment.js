d8d00780b4332d5f6502bd95c3ba5cf863ba8cb7
J0J0C0DING <102717784+J0J0C0DING@users.noreply.github.com>
2022-08-09 20:37:01 -0500

    Comments now post and display

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,29 +1,27 @@
 async function commentFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
-    const recipe_id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-    ];
+  const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
+  const recipe_id = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
 
-    if (comment_text) {
-        const response = await fetch('/api/comments', {
-            method: 'POST',
-            body: JSON.stringify({
-                recipe_id,
-                comment_text
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        
-        if (response.ok) {
-            document.location.reload();
-        } else {
-            alert(response.statusText);
-        }
+  if (comment_text) {
+    const response = await fetch('/api/comments', {
+      method: 'POST',
+      body: JSON.stringify({
+        recipe_id,
+        comment_text,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert(response.statusText);
     }
-};
+  }
+}
 
-document.querySelector('#add-comment-btn').addEventListener('submit', commentFormHandler);
+document.querySelector('#add-comment-btn').addEventListener('click', commentFormHandler);
