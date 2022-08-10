b25e7d719258e2ee5b58bc38b09cdb2666e5497d
J0J0C0DING <102717784+J0J0C0DING@users.noreply.github.com>
2022-08-09 20:47:50 -0500

    Comments recipe_id changed to avoid errors

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -2,7 +2,9 @@ async function commentFormHandler(event) {
   event.preventDefault();
 
   const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
-  const recipe_id = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
+
+  let recipe_id = document.querySelector('.comment-form');
+  recipe_id = recipe_id.id;
 
   if (comment_text) {
     const response = await fetch('/api/comments', {
